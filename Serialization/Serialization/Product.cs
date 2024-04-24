a685c8962ff9d06a7f309ecaeaff2690220e97ba
FuadMemmedov <tu7m43u29@code.edu.az>
2024-04-24 09:14:31 +0400

    task

---
+++ Serialization/Serialization/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    [Serializable]
+    internal class Product:ISerializable
+    {
+        static int _id;
+        public int Id { get;  }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+
+        public Product()
+        {
+            _id++;
+             Id = _id;
+        }
+
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("id", _id,typeof(Product));
+            info.AddValue("name", Name, typeof(Product));
+            info.AddValue("price",Price,typeof(Product));
+            info.AddValue("description", Description,typeof(Product));
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} Name: {Name} Price: {Price} Description: {Description}";
+        }
+    }
+}
---
+++ Serialization/Serialization/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Name = "Test";
+            product.Price = 10;
+            product.Description = "Test";
+
+
+            ProductToJson(product);
+
+            Console.WriteLine(JsonToProduct());
+          
+            
+           
+        }
+
+        public static void ProductToJson(Product product)
+        {
+            string path = "C:\\Users\\memme\\Desktop\\data.json";
+
+          using  Stream stream = new FileStream(path,FileMode.Create);
+
+
+            JsonSerializer.Serialize(stream, product);
+        }
+
+        public static Product JsonToProduct()
+        {
+            string path = "C:\\Users\\memme\\Desktop\\data.json";
+
+            using Stream stream = new FileStream(path, FileMode.Open);
+
+           return JsonSerializer.Deserialize<Product>(stream);
+
+        }
+    }
+}
